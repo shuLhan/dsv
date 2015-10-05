f36230fe409c91faaf78564c88bf2aac8b8c76fd
Mhd Sulhan <ms@kilabit.info>
2015-10-06 00:42:44 +0700

    Add Writer for handling writing DSV file using format metadata.

---
+++ writer.go
@@ -0,0 +1,196 @@
+package dsv
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var (
+	// ErrNoOutput tell you that no output file is given in config.
+	ErrNoOutput	= errors.New ("dsv: No output file is given in config")
+	// ErrNotOpen tell you that output file has not been opened yet.
+	ErrNotOpen	= errors.New ("dsv: Output file is not opened")
+	// ErrNilReader define an error when no Input file is given in JSON.
+	ErrNilReader	= errors.New ("dsv: Reader object is nil")
+)
+
+/*
+Writer write records from reader or slice using format configuration in
+metadata.
+*/
+type Writer struct {
+	// Output file where the records will be written.
+	Output		string		`json:"Output"`
+	// OutputMetadata define format for each field.
+	OutputMetadata	[]Metadata	`json:"OutputMetadata"`
+	// fWriter as write descriptor.
+	fWriter		*os.File
+	// BufWriter for buffered writer.
+	BufWriter	*bufio.Writer
+}
+
+/*
+NewWriter create a writer object.
+User must call Open after that to populate the output and metadata.
+*/
+func NewWriter () *Writer {
+	return &Writer {
+		Output		:"",
+		OutputMetadata	:nil,
+		fWriter		:nil,
+		BufWriter	:nil,
+	}
+}
+
+/*
+Open file for writing.
+*/
+func (writer *Writer) Open (fcfg string) (e error) {
+	cfg, e := ioutil.ReadFile (fcfg)
+
+	if nil != e {
+		log.Print ("dsv: ", e)
+		return e
+	}
+
+	e = writer.ParseConfig (cfg)
+
+	return nil
+}
+
+/*
+ParseConfig from JSON string.
+*/
+func (writer *Writer) ParseConfig (cfg []byte) (e error) {
+	e = json.Unmarshal ([]byte (cfg), writer)
+
+	if nil != e {
+		return
+	}
+
+	if "" == writer.Output {
+		return ErrNoOutput
+	}
+
+	e = writer.openOutput ()
+
+	return e
+}
+
+/*
+openOutput file and buffered writer.
+*/
+func (writer *Writer) openOutput () (e error) {
+	writer.fWriter, e = os.OpenFile (writer.Output,
+					os.O_CREATE | os.O_TRUNC | os.O_WRONLY,
+					0600)
+	if nil != e {
+		return e
+	}
+
+	writer.BufWriter = bufio.NewWriter (writer.fWriter)
+
+	return nil
+}
+
+/*
+Close all open descriptor.
+*/
+func (writer *Writer) Close () {
+	if nil != writer.BufWriter {
+		writer.BufWriter.Flush ()
+	}
+	if nil != writer.fWriter {
+		writer.fWriter.Close ()
+	}
+}
+
+/*
+WriteSlice dump content of slice to file using metadata format.
+*/
+func (writer *Writer) WriteSlice (records *[]Record) (e error) {
+	var md *Metadata
+	var r *Record
+	v := []byte{}
+
+	for i := range writer.OutputMetadata {
+		md = &writer.OutputMetadata[i]
+		r = &(*records)[i]
+
+		// no more record?
+		if nil == r {
+			break
+		}
+
+		if "" != md.LeftQuote {
+			v = append (v, []byte (md.LeftQuote)...)
+		}
+
+		v = append (v, (*r)...)
+
+		if "" != md.RightQuote {
+			v = append (v, []byte (md.RightQuote)...)
+		}
+
+		if "" != md.Separator {
+			v = append (v, []byte (md.Separator)...)
+		}
+	}
+
+	v = append (v, '\n')
+
+	_, e = writer.BufWriter.Write (v)
+
+	if nil != e {
+		return e
+	}
+
+	return nil
+}
+
+/*
+Write records from Reader to file.
+Return n for number of records written, and e for error that happened when
+writing to file.
+*/
+func (writer *Writer) Write (reader *Reader) (n int, e error) {
+	if nil == reader {
+		return 0, ErrNilReader
+	}
+	if nil == writer.fWriter {
+		return 0, ErrNotOpen
+	}
+
+	n = 0
+	row := reader.Records
+
+	for nil != row {
+		e = writer.WriteSlice (row.V)
+		if nil != e {
+			if DEBUG {
+				log.Println (e)
+			}
+		}
+		row = row.Next
+		n++
+	}
+
+	return n,nil
+}
+
+/*
+String yes, it will print it in JSON like format.
+*/
+func (writer *Writer) String() string {
+	r, e := json.MarshalIndent (writer, "", "\t")
+
+	if nil != e {
+		log.Print (e)
+	}
+
+	return string (r)
+}
---
+++ writer_test.go
@@ -0,0 +1,97 @@
+package dsv_test
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"testing"
+
+	"github.com/shuLhan/dsv"
+)
+
+/*
+doReadWrite test reading and writing the DSV data.
+*/
+func doReadWrite (dsvReader *dsv.Reader, dsvWriter *dsv.Writer, t *testing.T) {
+	exp	:= ""
+	i	:= 0
+	n 	:= 0
+	e	:= error (nil)
+
+	for {
+		n, e = dsvReader.Read ()
+
+		if n > 0 {
+			r := fmt.Sprint (dsvReader.Records)
+
+			if r != expectation[i] {
+				t.Error ("dsv_test: expecting\n", exp,
+					" got\n", r)
+			}
+
+			dsvWriter.Write (dsvReader)
+
+			i++
+		} else if e == io.EOF {
+			// EOF
+			break
+		}
+	}
+}
+
+/*
+TestWriter test reading and writing DSV.
+*/
+func TestWriter (t *testing.T) {
+	if DEBUG {
+		log.Println (">>> TestWriter")
+	}
+
+	// Initialize dsv reader
+	dsvReader := dsv.NewReader ()
+
+	e := dsvReader.Open ("config.dsv")
+
+	if nil != e {
+		t.Error (e)
+	}
+
+	defer dsvReader.Close ()
+
+	// Initialize dsv writer
+	dsvWriter := dsv.NewWriter ()
+
+	e = dsvWriter.Open ("config.dsv")
+
+	if nil != e {
+		t.Error (e)
+	}
+
+	if DEBUG {
+		log.Print (dsvWriter)
+	}
+
+	doReadWrite (dsvReader, dsvWriter, t)
+	dsvWriter.Close ()
+
+	// Compare the ouput from Writer
+	out, e := ioutil.ReadFile (dsvWriter.Output)
+
+	if nil != e {
+		t.Error (e)
+	}
+
+	exp, e := ioutil.ReadFile ("expected.dsv")
+
+	if nil != e {
+		t.Error (e)
+	}
+
+	r := bytes.Compare (out, exp)
+
+	if 0 != r {
+		t.Error ("Output different from expected (", r ,")")
+	}
+}
