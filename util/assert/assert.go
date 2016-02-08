75cc5c1328303a94cf581fd61c367be205dd59a9
Mhd Sulhan <ms@kilabit.info>
2016-02-08 23:44:59 +0700

    util.assert: pretty format error message to make it easy to compare.

--- util/assert/assert.go
+++ util/assert/assert.go
@@ -18,7 +18,7 @@ import (
 func isEqual(t *testing.T, exp, got interface{}, equal bool) {
 	if reflect.DeepEqual(exp, got) != equal {
 		debug.PrintStack()
-		t.Fatal("Expecting", exp, "got", got)
+		t.Fatalf("Expecting\n>>> '%v', got\n>>> '%v'\n", exp, got)
 	}
 }
 
