28fe0a070548df0c25e2151d13bfdcf64ec335d4
Mhd Sulhan <ms@kilabit.info>
2016-02-10 22:13:24 +0700

    metadata: add function to find metadata in slice of metadata.

--- metadatainterface.go
+++ metadatainterface.go
@@ -20,3 +20,25 @@ type MetadataInterface interface {
 
 	IsEqual(MetadataInterface) bool
 }
+
+/*
+FindMetadata Given a slice of metadata, find `mdin` in the slice which has the
+same name, ignoring metadata where Skip value is true.
+If found, return the index and metadata object of matched metadata name.
+If not found return -1 as index and nil in `mdout`.
+*/
+func FindMetadata(mdin MetadataInterface, mds []MetadataInterface) (
+	idx int,
+	mdout MetadataInterface,
+) {
+	for _, md := range mds {
+		if md.GetName() == mdin.GetName() {
+			mdout = md
+			break
+		}
+		if !md.GetSkip() {
+			idx++
+		}
+	}
+	return idx, mdout
+}
