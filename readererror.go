05d708392f97c591fe7368f2359361b734dc1f03
Shulhan <ms@kilabit.info>
2018-03-16 15:23:07 +0700

    Update copyright years

--- readererror.go
+++ readererror.go
@@ -1,4 +1,4 @@
-// Copyright 2016 Mhd Sulhan <ms@kilabit.info>. All rights reserved.
+// Copyright 2015-2018, Shulhan <ms@kilabit.info>. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
